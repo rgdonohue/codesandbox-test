4ae97475070518966869fbd7326b8a49c7633ba8
Rich Donohue <rgdonohue@gmail.com>
2020-08-25 16:30:42 -0600

    testing this silly thing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
 
-import App from "./App";
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoicmdkb25vaHVlIiwiYSI6Im5Ua3F4UzgifQ.PClcVzU5OUj17kuxqsY_Dg";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+class Application extends React.Component {
+  mapRef = React.createRef();
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 1.5
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapRef.current,
+      style: "mapbox://styles/mapbox/streets-v9",
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on("move", () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
+  render() {
+    const { lng, lat, zoom } = this.state;
+
+    return (
+      <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={this.mapRef} className="absolute top right left bottom" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
